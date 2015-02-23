19dc839b21234d2b2f8de19d38f22d02257d89e5
sowjanya <sowju19@gmail.com>
2015-02-22 22:01:17 -0800

    added user image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223012939) do
+ActiveRecord::Schema.define(version: 20150223054948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20150223012939) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.string   "image"
   end
 
 end
